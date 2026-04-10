87581dbae309708fdea043630a3daf3c3dc0c699
qq_62874680 <3220251360@bit.edu.cn>
2026-04-11 01:23:13 +0800

    作业1完整

--- tests/adapters.py
+++ tests/adapters.py
@@ -476,7 +476,9 @@ def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
         Float[Tensor,"..."]: of with the same shape as `in_features` with the output of applying
         SiLU to each element.
     """
-    raise NotImplementedError
+    from cs336_basics.module import silu
+
+    return silu(in_features)
 
 
 def run_get_batch(
